e6931c93da723464978b5d950b9a11a99364e6d1
Marcel Mulder <mmulder@victronenergy.com>
2014-05-19 09:04:55 +0200

    app: cleanup

--- app.cpp
+++ app.cpp
@@ -10,7 +10,6 @@ App::App(QObject *parent) :
 	connect(&mServer, SIGNAL(modbusRequest(ADU*const)), &mBackend, SLOT(modbusRequest(ADU*const)));
 	connect(&mBackend, SIGNAL(modbusReply(ADU*const)), &mServer, SLOT(modbusReply(ADU*const)));
 	connect(&mBackend, SIGNAL(getValues(int,int,int,QByteArray&)),&mMapping, SLOT(getValues(int,int,int,QByteArray&)));
-	//connect(&mDBusServices, SIGNAL(dbusServiceFound(DBusService*)), &mMapping, SLOT(dbusServiceFound(DBusService*)));
 
 	mDBusServices.initialScan();
 }
